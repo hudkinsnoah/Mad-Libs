b605982b84fcb05f36fa49cc08df9e6bb353b6ea
Noah <hudkinsn@oregonstate.edu>
2023-11-07 20:52:18 -0800

    Updating variable name

--- src/Play.py
+++ src/Play.py
@@ -65,39 +65,39 @@ class Play:
             user_input = self.CheckForInput(self.MadLib[self.PromptsList[prompt][0] + 1:self.PromptsList[prompt][1]])
             self.PromptsList[prompt].append(user_input)
 
-    def CheckForInput(self, type):
+    def CheckForInput(self, given_type):
         valid_input = False
         while not valid_input:
             user_input = input()
-            if type == "adjective":
+            if given_type == "adjective":
                 if user_input.strip().lower() not in self.adjectives:
                     print("Does not appear to be a valid adjective. Please try again")
                     continue
-            elif type == "animal" or type == "type of bug":
+            elif given_type == "animal" or given_type == "type of bug":
                 if user_input.strip().lower() not in self.animals:
                     print("Does not appear to be a valid adjective. Please try again")
                     continue
-            elif type == "part of the body" or type == "body part":
+            elif given_type == "part of the body" or given_type == "body part":
                 if user_input.strip().lower() not in self.bodyparts:
                     print("Does not appear to be a valid part of the body. Please try again")
                     continue
-            elif type == "clothing item":
+            elif given_type == "clothing item":
                 if user_input.strip().lower() not in self.clothes:
                     print("Does not appear to be a valid clothing item. Please try again")
                     continue
-            elif type == "noun":
+            elif given_type == "noun":
                 if user_input.strip().lower() not in self.nouns:
                     print("Does not appear to be a valid noun. Please try again")
                     continue
-            elif type == "verb":
+            elif given_type == "verb":
                 if user_input.strip().lower() not in self.verbs:
                     print("Does not appear to be a valid verb. Please try again")
                     continue
-            elif "ending in \"ing\"" in type:
+            elif "ending in \"ing\"" in given_type:
                 if not user_input.lower().endswith("ing"):
                     print("Does not end with 'ing'. Please try again")
                     continue
-            elif type == "number":
+            elif given_type == "number":
                 if not user_input.isnumeric():
                     print("Does not appear to be a valid number. Please try again")
                     continue
